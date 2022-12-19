baf703f13afaa92b579e888c80da77b0d31ef316
azizp128 <azizprabowo128@gmail.com>
2022-12-19 22:19:58 +0700

    fixed some JS error syntaxs

--- static/script.js
+++ static/script.js
@@ -1,41 +1,52 @@
-const chatBox = document.querySelector(".chatBox");
 const inputBox = document.querySelector(".input-text");
 const emoteList = document.querySelectorAll(".emote");
 const cardTitle = document.querySelector(".card__title");
 const sendBtn = document.querySelector(".btn");
 const errorIcon = document.querySelector(".icon-error");
 const errorMessage = document.querySelector(".error-message");
-
-function createAndShowElement(inpVal, outVal) {
-  const input = document.createElement("span");
-  input.className = "input";
-  input.innerHTML = `Input : ${inpVal}`;
-
-  const output = document.createElement("span");
-  output.className = "output";
-  output.innerHTML = `Output : ${outVal}`;
-
-  const result = document.createElement("div");
-  result.className = "result";
-  result.style.display = "flex";
-
-  result.appendChild(input);
-  result.appendChild(output);
-  chatBox.appendChild(result);
-
-  chatBox.style.display = "flex";
-
-  if (inputBox.classList.contains("input-error")) {
-    inputBox.classList.remove("input-error");
-    errorIcon.classList.add("hidden");
-  }
-}
+const chatBox = document.querySelector(".chatBox");
 
 function removeEmoteList() {
   emoteList.forEach(function (emote) {
     emote.style.display = "none";
     cardTitle.style.display = "none";
   });
+}
+
+// function createNewElements() {
+//   const result = document.createElement("div");
+//   result.className = "result";
+//   result.style.display = "flex";
+
+//   const input = document.createElement("span");
+//   const output = document.createElement("span");
+
+//   result.appendChild(input);
+//   result.appendChild(output);
+//   chatBox.appendChild(result);
+
+//   input.className = "input";
+//   output.className = "output";
+
+//   chatBox.style.display = "flex";
+
+//   if (inputBox.classList.contains("input-error")) {
+//     inputBox.classList.remove("input-error");
+//     errorIcon.classList.add("hidden");
+//   }
+//   input.innerHTML = `Input : ${inputBox.value}`;
+//   output.innerHTML = `Output : ${myVariable}`;
+// }
+
+function validateFields() {
+  // Check presence of values
+  if (inputBox.value.trim() === "") {
+    errorIcon.classList.remove("hidden");
+    inputBox.classList.add("input-error");
+  } else {
+    removeEmoteList();
+    createNewElements();
+  }
 }
 
 inputBox.addEventListener("keyup", (e) => {
@@ -48,14 +59,3 @@ sendBtn.addEventListener("click", (e) => {
   validateFields();
   e.preventDefault();
 });
-
-function validateFields() {
-  // Check presence of values
-  if (inputBox.value.trim() === "") {
-    errorIcon.classList.remove("hidden");
-    inputBox.classList.add("input-error");
-  } else {
-    removeEmoteList();
-    createAndShowElement(inputBox.value, chatBox.getAttribute("name"));
-  }
-}
